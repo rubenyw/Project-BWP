ecfff389fc01eb0743a13a1bdb6c84d3f61c6d81
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2022-11-21 20:21:00 +0700

    added register & myCss
    input utk register

---
+++ register.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="./mycss.css">
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="assets/css/general.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container px-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-6">
+                    <div class="text-center my-5">
+                        <div class="input-group">
+                            <input type="text" id="username" required class="input rounded">
+                            <label for="username" class="input-label">Username</label>
+                        </div>
+                        <br><br>
+                        <div class="input-group">
+                            <input type="text" id="email" required class="input rounded">
+                            <label for="email" class="input-label">Email Address</label>
+                        </div>
+                        <br><br>
+                        <div class="input-group">
+                            <input type="text" id="pass" required class="input rounded">
+                            <label for="pass" class="input-label">Password</label>
+                        </div>
+                        <br><br>
+                        <div class="input-group">
+                            <input type="text" id="confirm" required class="input rounded">
+                            <label for="confirm" class="input-label">Confirm Password</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
