0d95efed8603e524616320b4d8c873503d716b9b
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2022-11-21 20:23:48 +0700

    moved mycss content to general.css

--- register.php
+++ register.php
@@ -4,7 +4,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="./mycss.css">
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
