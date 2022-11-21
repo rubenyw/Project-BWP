199a42c854adc184ddf626e6b6fc187a8499e396
rubenyw <104652395+rubenyw@users.noreply.github.com>
2022-11-21 14:07:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  
+    require('action.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,12 +13,12 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <link href="assets/css/styles.css" rel="stylesheet">
+        <link href="assets/css/general.css" rel="stylesheet">
     </head>
     <body>
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-5">
+            <div class="container">
                 <a class="navbar-brand" href="">Job Finder</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -27,8 +27,9 @@
                         <li class="nav-item"><a class="nav-link" href="user_request.php">Request</a></li>
                         <li class="nav-item"><a class="nav-link" href="user_profile.php"></a></li>
                     </ul>
-                    <form class="d-flex ms-2" role="search">
-                        <a class="btn btn-outline-light btn-sm px-4" href="index.php">Log Out</a>
+                    <form class="d-flex" role="search">
+                        <a class="btn btn-outline-light btn-sm px-4 mx-4" href="login.php">Sign In</a>
+                        <a class="btn btn-outline-danger btn-sm px-4" href="register.php">Sign up</a>
                     </form>
                 </div>
             </div>
@@ -99,7 +100,7 @@
                                 <div class="card-body">
                                     
                                     <div class="table-responsive">
-                                        <table class="table table-borderless mb-0">
+                                        <table class="table table-borderless mb-0 align-items-center justify-content-center">
                                             <thead>
                                             <tr>
                                                 <th scope="col">No </th>
@@ -122,13 +123,13 @@
                                                     <td></td>
                                                     <td></td>
                                                     <td></td>
-                                                    <td>
-                                                        <form action='' method='post'>
+                                                    <td class="d-grid gap-2">
+                                                        <!-- <form action='' method='post'> -->
                                                             <input type='hidden' name='apply' value=''>
-                                                            <button type='submit' name='btn-apply' class='btn btn-outline-success btn-sm px-4'>
-                                                            Test
+                                                            <button name='btn-apply' class='btn btn-outline-success btn-sm px-4'>
+                                                                Apply
                                                             </button>
-                                                        </form>
+                                                        <!-- </form> -->
                                                     </td>
                                                 </tr>
                                               
@@ -156,12 +157,18 @@
         <script src="js/jquery-3.6.1.min.js"></script>
         <script>
             $(function(){
-                // let button = $("[name='btn-apply']");
-                // $(button).click(function(){
-                //     $(this).addClass("btn-outline-danger");
-                //     $(this).removeClass("btn-outline-success");
-                //     $(this).text("Pending")
-                //     })
+                let button = $("[name='btn-apply']");
+                $(button).click(function(){
+                    if($(this).text() == "Apply"){
+                        $(this).text("Pending")
+                        $(this).removeClass("btn-outline-success");
+                        $(this).addClass("btn-outline-danger");
+                    }else{
+                        $(this).text("Apply");
+                        $(this).removeClass("btn-outline-danger");
+                        $(this).addClass("btn-outline-success");
+                    }
+                })
                 let lokasi = $("#lokasi");
                 let jabatan = $("#jabatan");
                 let jenis = $("#jenis");
