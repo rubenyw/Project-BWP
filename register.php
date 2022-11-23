7c30c7f6f060a2a878f6c4c83a177b9c3d42e9b1
rubenyw <rubenyasonwinarta@gmail.com>
2022-11-23 12:41:18 +0700

    ubah javascript jadi jquery

--- register.php
+++ register.php
@@ -108,21 +108,20 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.6.1.min.js"></script>
     <script>
-        window.onload = (event) => {
-            formControl = document.querySelectorAll(".form");
-            for (let i = 0; i < formControl.length; i++) {
-                formControl[i].onblur = function(){
-                    if(formControl[i].value.length == 0){
-                        formControl[i].classList.add("is-invalid");
-                        formControl[i].classList.remove("is-valid");
-                    }else{
-                        formControl[i].classList.remove("is-invalid");
-                        formControl[i].classList.add("is-valid");
-                    }
+       $(function(){
+            formControl = $(".form");
+            $(formControl).blur(function(){
+                if($(this).val() == ""){
+                    $(this).addClass("is-invalid");
+                    $(this).removeClass("is-valid");
+                }else{
+                    $(this).addClass("is-valid");
+                    $(this).removeClass("is-invalid");
                 }
-            }
-        };
+            })
+        })
     </script>
 </body>
 </html>
